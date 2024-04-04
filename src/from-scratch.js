7d1d13b77f7100f38a50ef3236b60ecfb37a1b6e
Mekhi-M <mekhim0731@gmail.com>
2024-04-04 11:23:25 -0400

    q1 fromScratch done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,5 +1,20 @@
-const measureRain = () => {
+const measureRain = (inches) => {
+  if (inches === 0) {
+    console.log('drought');
+  } else if (inches < 2 && inches > 0) {
+    console.log('dry');
+  } else if (inches < 4 && inches >= 2) {
+    console.log('average');
+  } else if (inches < 6 && inches >= 4) {
+    console.log('rainy');
+  } else if (inches >= 6) {
+    console.log('flood');
+  }
 };
+
+// tests
+
+// measureRain(16);
 
 const happyBirthdayPet = () => {
 };
